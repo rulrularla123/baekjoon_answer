814b2eb2ad6813555c75720787cdae612f76ecd1
rulrularla123 <kimdh3700@gmail.com>
2025-06-05 13:20:36 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e=0;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    for(int i = 0; i<b; i++){
+        scanf("%d %d", &c, &d);
+        e += c*d;
+    }
+    if(a==e){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
