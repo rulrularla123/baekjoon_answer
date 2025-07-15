41b3cddbc4f75c3a63829ee5c216a2c9a93c504c
rulrularla123 <kimdh3700@gmail.com>
2025-07-15 23:32:29 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, temp;
+    scanf("%d %d %d", &a, &b, &c);
+    if(b > a){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if(c > a){
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if(c > b){
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    printf("%d %d %d", c, b, a);
+}
