2b645a7eb947a279ab1b81f187a6943e030c447e
rulrularla123 <kimdh3700@gmail.com>
2025-07-18 21:59:42 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a == b/c || a == b*c || a == b+c || a == b-c){
+        if(a == b-c) printf("%d=%d-%d", a, b, c);
+        else if(a == b*c) printf("%d=%d*%d", a, b, c);
+        else if(a == b+c) printf("%d=%d+%d", a, b, c);
+        else printf("%d=%d/%d", a, b, c);
+    }
+    else{
+        if(a-b==c) printf("%d-%d=%d", a, b, c);
+        else if(a*b==c) printf("%d*%d=%d", a, b, c);
+        else if(a+b==c) printf("%d+%d=%d", a, b, c);
+        else printf("%d/%d=%d", a, b, c);
+    }
+}
