adf82db4726c942f00474e52c949ebd21414a74f
rulrularla123 <kimdh3700@gmail.com>
2025-07-21 22:33:31 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 1188 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long long int be[10000] = {0, };
+
+long long int fibo(int c){
+    if(c < 2){
+        be[c] = c;
+        return be[c];
+    }
+    if(be[c] != 0) return be[c];
+    be[c] = fibo(c-1) + fibo(c-2);
+    return be[c];
+}
+
+int main() {
+    long long num;
+    scanf("%lld", &num);
+    fibo(num);
+    printf("%lld", be[num]);
+}
