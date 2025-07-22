5a2a9af9ee70e54faf7f74d07f20c210020f9d66
rulrularla123 <kimdh3700@gmail.com>
2025-07-22 17:49:28 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 1268 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long long int c0[10000] = {1, 0, 1, };
+long long int c1[10000] = {0, 1, 1, };
+
+int fibo(int c){
+    for(int i=2; i<=c; i++){
+        if(c0[i] == 0){
+            c0[i] =  c0[i-1] + c0[i-2];
+        }
+        if(c1[i] == 0){
+            c1[i] =  c1[i-1] + c1[i-2];
+        }
+    }
+    printf("%lld %lld\n", c0[c], c1[c]);
+}
+
+int main() {
+    int num, a;
+    scanf("%d", &num);
+    for(int i=0; i<num; i++){
+        scanf("%d", &a);
+        fibo(a);
+    }
+}
