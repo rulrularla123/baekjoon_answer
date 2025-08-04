235fe4691d7dd812f842458c895485c9773fde7f
rulrularla123 <kimdh3700@gmail.com>
2025-08-04 17:32:41 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 6448 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int arr[1000001], temp[1000001];
+
+void merge(int a[], int st, int en){
+     if (en - st <= 1) return;
+    int mid = (st + en) / 2;
+    merge(a, st, mid);
+    merge(a, mid, en);
+    int i = st, j = mid, k = st;
+    while(i < mid && j < en){
+        if (a[i] < a[j]) temp[k++] = a[i++];
+        else temp[k++] = a[j++];
+    }
+    while (i < mid) temp[k++] = a[i++];
+    while (j < en) temp[k++] = a[j++];
+    for (int t = st; t < en; t++) a[t] = temp[t];
+}
+
+int main() {
+    int le;
+    scanf("%d", &le);
+    for(int i=0; i<le; i++){
+        scanf("%d", &arr[i]);
+    }
+    merge(arr, 0, le);
+    for(int i=0; i<le; i++){
+        printf("%d ", arr[i]);
+    }
+}
