f8a70b7d26542b978f90b26a1323d33e18e3b116
rulrularla123 <kimdh3700@gmail.com>
2025-09-17 23:40:29 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char a[100000];
+    scanf("%s", a);
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n", a);
+    printf(":fan::fan::fan:\n");
+}
